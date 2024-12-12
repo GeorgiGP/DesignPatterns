3a7b93d00f16c6ff969216a3b0575af9e4dbe031
GeorgiLazov <georgilazov04@gmail.com>
2024-12-12 01:29:41 -0800

    Delete Figures/src directory

--- Figures/src/Main.java
+++
@@ -1,8 +0,0 @@
-import application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application application = new Application();
-        application.run();
-    }
-}
